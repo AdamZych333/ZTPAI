e5a101f998d55808a7ac1c5d12be76805f68ecbe
AdamZych333 <adam.zych99@wp.pl>
2021-05-01 17:28:07 +0200

    add SecurityController

--- migrations/Version20210501135527.php
+++ migrations/Version20210501135527.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210501124516 extends AbstractMigration
+final class Version20210501135527 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,15 +20,12 @@ final class Version20210501124516 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE meme ADD slug VARCHAR(255) NOT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_4B9F7934989D9B62 ON meme (slug)');
+
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP INDEX UNIQ_4B9F7934989D9B62');
-        $this->addSql('ALTER TABLE meme DROP slug');
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,31 +2,51 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/", name="login")
+     * @Route("/login", name="app_login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils,
+    UserRepository $userRepository
+    ): Response
     {
-        $message = '';
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
 
         return $this->render('Login/login.html.twig', [
-            'message' => $message
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
 
     /**
-     * @Route("/register", name="register")
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/register", name="app_register")
      */
     public function register(): Response
     {
         $message = '';
-
         return $this->render('Register/register.html.twig', [
             'message' => $message
         ]);
