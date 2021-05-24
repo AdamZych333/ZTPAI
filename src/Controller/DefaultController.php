6a5870621415a51eca49cc84dfc3dbc4bb33d146
AdamZych333 <adam.zych99@wp.pl>
2021-05-24 09:09:37 +0200

    add default controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MemeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     * @param MemeRepository $memeRepository
+     * @return Response
+     */
+    public function home(MemeRepository $memeRepository): Response{
+
+        return $this->render('Home/home.html.twig', [
+            'memes' => $memeRepository->findBy([], ['created_at' => 'ASC'])
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(): Response
+    {
+        return $this->render('Add/add.html.twig');
+    }
+
+    /**
+     * @Route("/top10", name="top10")
+     * @param MemeRepository $memeRepository
+     * @return Response
+     */
+    public function top10(MemeRepository $memeRepository): Response
+    {
+        return $this->render('TOP10/top10.html.twig', [
+            'memes' => $memeRepository->findBy([], ['likes' => 'DESC'])
+        ]);
+    }
+}
--- src/Controller/MemeController.php
+++ src/Controller/MemeController.php
@@ -37,38 +37,6 @@ class MemeController extends AbstractController
     }
 
     /**
-     * @Route("/home", name="home")
-     * @param MemeRepository $memeRepository
-     * @return Response
-     */
-    public function home(MemeRepository $memeRepository): Response{
-
-        return $this->render('Home/home.html.twig', [
-            'memes' => $memeRepository->findBy([], ['created_at' => 'ASC'])
-        ]);
-    }
-
-    /**
-     * @Route("/add", name="add")
-     */
-    public function add(): Response
-    {
-        return $this->render('Add/add.html.twig');
-    }
-
-    /**
-     * @Route("/top10", name="top10")
-     * @param MemeRepository $memeRepository
-     * @return Response
-     */
-    public function top10(MemeRepository $memeRepository): Response
-    {
-        return $this->render('TOP10/top10.html.twig', [
-            'memes' => $memeRepository->findBy([], ['likes' => 'DESC'])
-        ]);
-    }
-
-    /**
      * @Route("/meme/{slug}", name="meme")
      * @param Request $request
      * @param Meme $meme
